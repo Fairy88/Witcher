6d41b0d99d93f3c6184e7a81ca3c23e6eba0c046
Fairy88 <fairyice88@gmail.com>
2020-02-06 17:20:47 +0500

    responsive

--- docs/js/js.js
+++ docs/js/js.js
@@ -1,7 +1,19 @@
 var mySwiper = new Swiper('.swiper-container', {
-  slidesPerView: 2, //кол-во слайдов
+  slidesPerView: 1, //кол-во слайдов
   loop: true, //бесконечная прокрутка
   navigation: {
     nextEl: '.arrow',
   },
+  breakpoints: {
+    540: {
+      slidesPerView: 2
+    }
+  }
+});
+
+var menuButton = document.querySelector('.menu-btn');
+var menu = document.querySelector('.header');
+menuButton.addEventListener('click', function(){
+  menuButton.classList.toggle('menu-btn-active');//меняет гамбургер меню на крестик
+  menu.classList.toggle('header-active');//открывает медиа окно меню
 });
